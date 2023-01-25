9b781091b34cc087c8a158a373a0cfc1b6f68127
Alexander <121059969+AlexanderB-prog@users.noreply.github.com>
2023-01-25 21:39:53 +0300

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/navigation/main_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: Screens.main,
+      routes: MainNavigation().routes,
+    );
+  }
+}
+
---
+++ lib/navigation/main_navigation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/details_weather_screen.dart';
+import 'package:weather_app/screens/home_screen.dart';
+import 'package:weather_app/screens/weather_screen.dart';
+
+
+abstract class Screens {
+  static const main = '/';
+  static const weatherScreen = '/weather_screen';
+  static const detailsScreen = '/weather_page/details_screen';
+}
+
+class MainNavigation {
+  Map<String, WidgetBuilder> get routes => {
+        Screens.main: (context) => const HomeScreen(),
+        Screens.weatherScreen: (context) => const WeatherScreen(),
+        Screens.detailsScreen: (context) => const DetailsWeatherScreen(),
+      };
+}
